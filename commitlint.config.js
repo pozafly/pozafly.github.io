25a0423c67272067097c51b9278797ab2ef35370
pozafly <pozafly@gmail.com>
2023-10-02 17:04:42 +0900

    chore: [ci skip] commitlint type-enum setting modify

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,5 +4,23 @@ module.exports = {
     'header-max-length': [0],
     'body-max-line-length': [0],
     'footer-max-line-length': [0],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'post',
+      ],
+    ],
   },
 };
