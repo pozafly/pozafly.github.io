2e0d972a29098d83a7c7b82ec7cbe50ac8190c86
pozafly <pozafly@gmail.com>
2023-10-07 12:15:41 +0900

    chore: postCSS 관련 plugin 제거.
    - emotion을 사용하기 때문에 emotion plugin에서 autoprefixer 및 postCSS 관련 작업을 모두 해주고 있음.

--- gatsby-config.js
+++ gatsby-config.js
@@ -150,12 +150,6 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-postcss',
-      options: {
-        postCssPlugins: [require('postcss-color-function'), require('cssnano')()],
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
